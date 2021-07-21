ad8369d2fda89d34a0c0722386471f8a79fe4fc1
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2021-07-21 10:42:56 -0400

    added classroom code to project

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import com.HighLow.HighLow;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Yahtzee game = new Yahtzee();
+        game.play();
+    }
+}
